04da13c71b32c33b57da8dcb88e16e399fe06944
hnerud <hnerud@applesauseinteractive.com>
2015-07-20 17:18:51 -0500

    TriangleAroundBase

---
+++ Chapter5-4/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace StartingGraphics
+{
+    public partial class Form1 : Form
+    {
+        FastLoop _fastLoop;
+        bool _fullscreen = false;
+
+        public Form1()
+        {
+            _fastLoop = new FastLoop(GameLoop);
+
+            InitializeComponent();
+            _openGLControl.InitializeContexts();
+
+
+            if (_fullscreen)
+            {
+                FormBorderStyle = FormBorderStyle.None;
+                WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        void GameLoop(double elapsedTime)
+        {
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glPointSize(5.0f);
+            Gl.glRotated(10 * elapsedTime, 1, 0, 0);
+            Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
+            {
+                Gl.glColor4d(1.0, 0.0, 0.0, 0.5);
+                Gl.glVertex3d(-0.5, 0, 0);
+                Gl.glColor3d(0.0, 1.0, 0.0);
+                Gl.glVertex3d(0.5, 0, 0);
+
+                Gl.glColor3d(0.0, 0.0, 1.0);
+                Gl.glVertex3d(0, 0.5, 0);
+
+            }
+            Gl.glEnd();
+            Gl.glFinish();
+
+            _openGLControl.Refresh();
+        }
+    }
+
+}
