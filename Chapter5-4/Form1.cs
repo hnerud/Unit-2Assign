58434b085f13df9aeae5e2bcb992fdb561d241d8
hnerud <hnerud@applesauseinteractive.com>
2015-07-20 18:27:07 -0500

    SQUARE AROUND BASE

--- Chapter5-4/Form1.cs
+++ Chapter5-4/Form1.cs
@@ -39,13 +39,19 @@ namespace StartingGraphics
             Gl.glRotated(10 * elapsedTime, 1, 0, 0);
             Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
             {
-                Gl.glColor4d(1.0, 0.0, 0.0, 0.5);
-                Gl.glVertex3d(-0.5, 0, 0);
-                Gl.glColor3d(0.0, 1.0, 0.0);
-                Gl.glVertex3d(0.5, 0, 0);
 
+                //TRIANGLE 1
+                Gl.glColor4d(1.0, 0.0, 0.0, 0.5);
+                Gl.glVertex3d(-0.5, 0, 0);//BOTTOM LEFT
+                Gl.glColor3d(0.0, 1.0, 0.0);
+                Gl.glVertex3d(-0.5, 1, 0);//TOP LEFT
                 Gl.glColor3d(0.0, 0.0, 1.0);
-                Gl.glVertex3d(0, 0.5, 0);
+                Gl.glVertex3d(1, 0, 0);//BOTTOM RIGHT
+
+                //TRIANGLE2
+
+                Gl.glColor3d(0.5, 0.0, 0.5);
+                Gl.glVertex3d(1, 1.0, 0);//TOP RIGHT
 
             }
             Gl.glEnd();
