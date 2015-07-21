9cf4c14237a4c0c5813b1155d1659a8cd4f579e7
hnerud <hnerud@applesauseinteractive.com>
2015-07-20 19:13:22 -0500

    Hexagon rotated around the center

--- Chapter5-4/Form1.cs
+++ Chapter5-4/Form1.cs
@@ -35,23 +35,27 @@ namespace StartingGraphics
             Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glColor3d(1.0, 0.0, 0.0);
             Gl.glPointSize(5.0f);
-            Gl.glRotated(10 * elapsedTime, 1, 0, 0);
-            Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
+            Gl.glRotated(10 * elapsedTime, 0, 0, 1);
+            Gl.glBegin(Gl.GL_POLYGON);
             {
 
-                //TRIANGLE 1
-                Gl.glColor4d(1.0, 0.0, 0.0, 0.5);
-                Gl.glVertex3d(-0.5, 0, 0);//BOTTOM LEFT
-                Gl.glColor3d(0.0, 1.0, 0.0);
-                Gl.glVertex3d(-0.5, 1, 0);//TOP LEFT
-                Gl.glColor3d(0.0, 0.0, 1.0);
-                Gl.glVertex3d(1, 0, 0);//BOTTOM RIGHT
-
-                //TRIANGLE2
-
-                Gl.glColor3d(0.5, 0.0, 0.5);
-                Gl.glVertex3d(1, 1.0, 0);//TOP RIGHT
+                
+               
+                Gl.glVertex3d(-0.25, 0.5, 0);//TOP LEFT
+               
+                Gl.glVertex3d(0.25, 0.5, 0);//TOP RIGHT
+               
+                Gl.glVertex3d(0.5, 0, 0);//RIGHT SIDE
+                
+                Gl.glVertex3d(0.25, -0.5, 0);//BOTTOM RIGHT
+              
+                Gl.glVertex3d(-.25, -0.5, 0);//BOTTOM LEFT
+             
+                Gl.glVertex3d(-0.5, 0, 0);//LEFT SIDE
+                
+               
 
             }
             Gl.glEnd();
